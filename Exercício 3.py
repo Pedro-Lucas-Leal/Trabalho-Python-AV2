ed2dd05d2066f1aec61606a2576f610c6efc29f0
Pedro-Lucas-Leal <104956862+Pedro-Lucas-Leal@users.noreply.github.com>
2022-11-07 16:30:23 -0300

    Update Exercício 3.py

--- Exercício 3.py
+++ Exercício 3.py
@@ -1,22 +1,29 @@
-def Converter (horas, minutos):
-    if horas >= 0 and horas < 12 or horas == 24 and minutos >=0 and minutos < 60 :
+def Converter(horas, minutos):
+    if horas >= 0 and horas < 12 or horas == 24 and minutos >= 0 and minutos < 60:
         if horas == 24 and minutos >= 0 and minutos < 60:
             horas = 0
-        print(f'{horas}:{minutos} {A}')
-    elif horas >= 12 and horas <24 and minutos >= 0 and minutos < 60:
+        Imprimir(horas, minutos, A)
+    elif horas >= 12 and horas < 24 and minutos >= 0 and minutos < 60:
         if horas > 12:
             horas = horas - 12
+        Imprimir(horas, minutos, P)
+    else:
+        Imprimir(horas, minutos, 0)
+
+def Imprimir(horas, minutos, nomenclatura):
+    if nomenclatura == A:
+        print(f'{horas}:{minutos} {A}')
+    elif nomenclatura == P:
         print(f'{horas}:{minutos} {P}')
     else:
         print("Horário inválido, tente novamente colocando a hora de 0 à 24 e os minutos de 0 à 59.")
-            
+
 while True:
     horas = int(input("Insira a hora no modelo 24h: "))
     minutos = int(input("Agora coloque os minutos: "))
     A = 'A.M.'
     P = 'P.M.'
     if horas >= 0 and minutos >= 0:
-        Converter(horas,minutos)
-        
+        Converter(horas, minutos)
     else:
         break
